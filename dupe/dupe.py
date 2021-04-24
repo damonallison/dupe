68101ed87a004764a5025befa85f4977eaf66b18
Damon Allison <damon@damonallison.com>
2021-04-23 21:41:02 -0500

    A more user-friendly dupe is coming

--- dupe/dupe.py
+++ dupe/dupe.py
@@ -2,19 +2,48 @@ import argparse
 import os
 import hashlib
 from typing import Dict, List
+import logging
+from pathlib import Path
 
-
+#
+# 1. Iterate directory
+#
+#    * number of files
+#    * total size
+#
+# 2. md5 file
+#
+# 3. Add to dupe list
+#
+# 4. Decrement operation.
+#
+#
 def md5(fpath: str) -> str:
     """Computes the md5 hash of a file"""
     blocksize = 65536
     md5 = hashlib.md5()
-    with open(fpath, 'rb') as f:
-        for block in iter(lambda: f.read(blocksize), b''):
+    with open(fpath, "rb") as f:
+        for block in iter(lambda: f.read(blocksize), b""):
             md5.update(block)
     return md5.hexdigest()
 
 
-def find(root: str = '.') -> Dict[str, List[str]]:
+class DupeOperation:
+    def __init__(self, root: Path):
+        if not root.exists() or not root.is_dir():
+            raise ValueError
+        self.root = root.resolve(strict=True)
+
+    def reset(self) -> None:
+        self.remaining: List[Path] = []
+        for path, _, files in os.walk(self.root):
+            for fname in files:
+                p = Path(path, fname)
+                s = p.stat()
+                self.remaining.append(Path(path, fname))
+
+
+def find(root: str = ".") -> Dict[str, List[str]]:
     """Finds duplicate files.
 
     Returns:
@@ -22,6 +51,7 @@ def find(root: str = '.') -> Dict[str, List[str]]:
 
       {"md5" : ["./file1.txt", "./file2.txt"]}
     """
+    logging.info(f"starting find at {root}")
     d = dict()
     for path, _, files in os.walk(root):
         for fname in files:
@@ -38,8 +68,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "dir",
-        nargs='?',
-        default='.',
-        help="the root directory to search for dupes. defaults to the current directory.")
+        nargs="?",
+        default=".",
+        help="the root directory to search for dupes. defaults to the current directory.",
+    )
     args = parser.parse_args()
     print(find(args.dir))
--- tests/test_dupe.py
+++ tests/test_dupe.py
@@ -1,5 +1,22 @@
-from dupe import __version__
+import logging
+import os
+from pathlib import Path
+from typing import Dict, List
+from dupe import dupe
 
+#
+# What is the difference between os.path and pathlib.Path?
+#
+# * os.path: lower level
+#
+class TestDupe:
+    def print_Path(path: Path) -> None:
+        pass
 
-def test_version():
-    assert __version__ == '0.0.2'
+    def create_test_dir(root: Path) -> None:
+        pass
+
+    def test_find_dups(self) -> None:
+        d = dupe.find(os.getcwd())
+        for k, v in d.items():
+            logging.debug(f"{k} = {v}")
